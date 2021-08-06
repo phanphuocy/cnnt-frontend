b02e21c5b18ee220e59da71c767db1de75584031
Phan Phước Ý <phanphuocy@gmail.com>
2021-08-06 15:20:01 +0700

    fix the graphql url in the apollo client config

--- config/apollo-client.js
+++ config/apollo-client.js
@@ -1,8 +1,10 @@
-import { API_URL } from '.';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri:
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_GRAPHQL_URL
+      : 'http://localhost:5000/graphql',
   cache: new InMemoryCache(),
 });
 
